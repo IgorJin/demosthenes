fce0d2577649ee5990d7534edc55eede74ba9640
igor.z <igor.z@fora-soft.com>
2021-02-24 20:55:36 +0300

    Updates

---
+++ precache-manifest.44e14f1e6d9c8dd9fbaedf4d2d682017.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efe95503a12d3cc52e7b0b49c04520e",
+    "url": "/personal_cabinet/index.html"
+  },
+  {
+    "revision": "91faecd5d89a55bd9622",
+    "url": "/personal_cabinet/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "4f9c1a752a6169e3c2bd",
+    "url": "/personal_cabinet/static/css/main.839f138b.chunk.css"
+  },
+  {
+    "revision": "91faecd5d89a55bd9622",
+    "url": "/personal_cabinet/static/js/2.e6cce154.chunk.js"
+  },
+  {
+    "revision": "baeb6afce6491b01f59742a9fba2ffc2",
+    "url": "/personal_cabinet/static/js/2.e6cce154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9c1a752a6169e3c2bd",
+    "url": "/personal_cabinet/static/js/main.b233a647.chunk.js"
+  },
+  {
+    "revision": "0350ce5a628f3b55e905",
+    "url": "/personal_cabinet/static/js/runtime-main.1789d26f.js"
+  },
+  {
+    "revision": "1b5b0267d68281c45c4820ef7f2e1a58",
+    "url": "/personal_cabinet/static/media/comments.1b5b0267.svg"
+  },
+  {
+    "revision": "b77a3eabb58430b764af3f5d273eadf9",
+    "url": "/personal_cabinet/static/media/contacts.b77a3eab.svg"
+  },
+  {
+    "revision": "0c068b418d1dad3aa9f3ea6bffe6b842",
+    "url": "/personal_cabinet/static/media/dashboard.0c068b41.svg"
+  },
+  {
+    "revision": "6e86a9719a0025ba2e37ec1ee0952127",
+    "url": "/personal_cabinet/static/media/lisa.6e86a971.svg"
+  },
+  {
+    "revision": "4761413c9a19df7058af690c849cdad6",
+    "url": "/personal_cabinet/static/media/meetings.4761413c.svg"
+  },
+  {
+    "revision": "7a89dcb8a7ad51fd03382b9f1ce67251",
+    "url": "/personal_cabinet/static/media/profile.7a89dcb8.svg"
+  },
+  {
+    "revision": "22c10d29a49d5488a7f5aabc234bafbc",
+    "url": "/personal_cabinet/static/media/search.22c10d29.svg"
+  },
+  {
+    "revision": "3725a60ec7fcd17acd802ddf0602f262",
+    "url": "/personal_cabinet/static/media/task.3725a60e.svg"
+  },
+  {
+    "revision": "4bdf88d4bd4afc5e5aafdf0936d77008",
+    "url": "/personal_cabinet/static/media/todo_delete.4bdf88d4.svg"
+  },
+  {
+    "revision": "1ada50f27cf17e277b162e43a22572f2",
+    "url": "/personal_cabinet/static/media/todo_edit.1ada50f2.svg"
+  }
+]);
